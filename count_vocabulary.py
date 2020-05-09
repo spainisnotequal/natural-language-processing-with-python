55e2976173659c6ddf13f5c454f5d278e4834d4a
spainisnotequal <spainisnotequal@gmail.com>
2020-05-09 19:20:52 +0200

    Get the frequency of a word in a text

--- count_vocabulary.py
+++ count_vocabulary.py
@@ -2,13 +2,23 @@ from nltk.book import text3 as book_of_genesis
 
 number_of_tokens = len(book_of_genesis)
 unique_tokens = set(book_of_genesis)
+number_of_unique_tokens = len(unique_tokens)
 unique_tokens_sorted = sorted(unique_tokens)
 
 
 def lexical_diversity(text):
-    "A mesaure of the lexical diversity (or richness) of a text."
+    "Compute a mesaure of the lexical diversity (or richness) of a text."
     return len(set(text)) / len(text)
 
 
 print("lexical diversity:",
       round(lexical_diversity(book_of_genesis) * 100, 2), "%")
+
+
+def word_frequency(word, text):
+    "Compute the frequency that a word appears in a text."
+    return text.count(word) / len(text)
+
+
+print("frequency of 'a':",
+      round(word_frequency("a", book_of_genesis) * 100, 2), "%")
